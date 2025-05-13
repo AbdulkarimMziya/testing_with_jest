147b1a11413363122a836437e2b6679f285c60c6
AbdulkarimMziya <abdulmziya@gmail.com>
2025-05-13 16:27:19 -0700

    test: add basic operations tests for calculator module

--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./script";
+import { capitalize, reverseString, calculator } from "./script";
 
 test('Capitalize first leter', () => {
   expect(capitalize("logan")).toBe("Logan")
@@ -6,4 +6,20 @@ test('Capitalize first leter', () => {
 
 test('Reverse a string', () => {
     expect(reverseString("Hello World")).toBe("dlroW olleH")
+})
+
+test("Sum: 10 + 5", () => {
+    expect(calculator.add(10,5)).toBe(15)
+})
+
+test("Subtract: 10 - 5", () => {
+    expect(calculator.subtract(10,5)).toBe(5)
+})
+
+test("Divide: 10 / 5", () => {
+    expect(calculator.divide(10,5)).toBe(2)
+})
+
+test("Multiply: 10 * 5", () => {
+    expect(calculator.multiply(10,5)).toBe(50)
 })
